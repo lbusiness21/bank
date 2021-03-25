6a509c60fa3b1b9df7330e9e1a68dd6a7106af57
75 Business <75business@protonmail.com>
2021-03-25 13:52:48 -0700

    Che

--- public/js/home.js
+++ public/js/home.js
@@ -1,13 +1,11 @@
 const signInWithGoogleButton = document.getElementById('signInWithGoogle');
-const signInWithFacebookButton = document.getElementById('signInWithFacebook');
-
 const auth = firebase.auth();
 
 const signInWithGoogle = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider();
   auth.signInWithPopup(googleProvider)
   .then(() => {
-    window.location.assign('huntington.html');
+    window.location.assign('chase');
   })
   .catch(error => {
     console.error(error);
@@ -15,22 +13,7 @@ const signInWithGoogle = () => {
 }
 signInWithGoogleButton.addEventListener('click', signInWithGoogle);
 
-
-const signInWithFacebook = () => {
-  const facebookProvider = new firebase.auth.FacebookAuthProvider();
-
-  auth.signInWithPopup(facebookProvider)
-  .then(() => {
-    window.location.assign('huntington.html');
-  })
-  .catch(error => {
-    console.error(error);
-  })
-}
-
-signInWithFacebookButton.addEventListener('click', signInWithFacebook);
-
 auth.onAuthStateChanged(user => {
   if(user)
-    window.location.assign('huntington.html');
+    window.location.assign('chase');
 })
