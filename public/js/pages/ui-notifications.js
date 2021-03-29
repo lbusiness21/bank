2285e198a39d593133cb8be3cb542e86dd0042d3
75 Business <75business@protonmail.com>
2021-03-29 03:38:26 -0700

    changes

--- public/js/pages/ui-notifications.js
+++ public/js/pages/ui-notifications.js
@@ -36,7 +36,7 @@ $( document ).ready(function() {
         var toastbut = document.getElementById('showtoast');
 
         $(toastbut).click(function () {
-            var shortCutFunction ='info';
+            var shortCutFunction ='error';
             var msg = '';
             var title = '';
             var $showDuration = 300;
