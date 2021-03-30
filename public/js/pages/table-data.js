06ddf45b2ef5419f8e6cbace16144895f649099a
75 Business <75business@protonmail.com>
2021-03-30 03:20:02 -0700

    chan

--- public/js/pages/table-data.js
+++ public/js/pages/table-data.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
             { "visible": false, "targets": 2 }
         ],
         "order": [[ 2, 'asc' ]],
-        "displayLength": 10,
+        "displayLength": 25,
         "drawCallback": function ( settings ) {
             var api = this.api();
             var rows = api.rows( {page:'current'} ).nodes();
@@ -50,12 +50,16 @@ $(document).ready(function() {
     $('#add-row').on( 'click', function () {
         if($("#add-row-form").isValid()) {
             var name = $('#name-input').val(),
+                position = $('#position-input').val(),
+                age = $('#age-input').val(),
                 date = $('#date-input').val(),
                 salary = $('#salary-input').val();
             t.row.add( [
                 name,
+                position,
+                age,
                 date,
-                salary
+                '$' + salary
             ] ).draw();
             
             $('.modal').modal('hide');
