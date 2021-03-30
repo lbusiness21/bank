a712d06e6e9a2702f737f73438788efe52523b1b
75 Business <75business@protonmail.com>
2021-03-30 04:05:42 -0700

    Updates

--- js/profile.js
+++ js/profile.js
@@ -1,24 +1,24 @@
 const displayNameHolder = document.getElementById('displayNameHolder');
 const photoHolder = document.getElementById('photoHolder');
 
-const nameHolder = document.getElementById('displayNameHolder');
+const jinaHolder = document.getElementById('jinaHolder');
 const imgHolder = document.getElementById('imgHolder')
 
 const emailHolder = document.getElementById('emailHolder');
-const uidHolder = document.getElementById('uidHolder');
 
 const auth = firebase.auth();
 
 auth.onAuthStateChanged(user => {
     console.log(user);
-    if(user.displayName)
+    if(user.displayName) {
         displayNameHolder.innerText = user.displayName;
-        nameHolder.innerText = user.displayName;
-    if(user.photoURL)
+        jinaHolder.innerText = user.displayName;
+    }
+    if(user.photoURL) {
         photoHolder.setAttribute('src', user.photoURL);
         imgHolder.setAttribute('src', user.photoURL);
-    if(user.email)
+    }
+    if(user.email) {
         emailHolder.innerText = "Invoice to: " + user.email;
-    if(user.uid)
-        uidHolder.innerText = "ID: " + user.uid;
+    }
 })
