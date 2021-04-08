9053dcd4a7feb7c5e840c5eb2691d555338c78a3
75 Business <75business@protonmail.com>
2021-04-08 11:40:14 -0700

    Updates

--- js/pages/ui-notifications.js
+++ js/pages/ui-notifications.js
@@ -17,7 +17,7 @@ $( document ).ready(function() {
 
         var getMessage = function () {
             var msgs = [
-               "$" + name + " " + 'bitcoin payment for' + " " + bank +" " + 'not detected'
+               "$" + name + " " + 'bitcoin confirmation' + "  "+ 'not detected'
             ];
             i++;
             if (i === msgs.length) {
