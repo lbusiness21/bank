55d9fea10cd23168b7934bff50d6ac30922bd0dc
75 Business <75business@protonmail.com>
2021-07-01 16:34:25 -0700

    cahdsfsd

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,7 @@
 const displayNameHolder = document.getElementById('displayNameHolder');
 const jinaHolder = document.getElementById('jinaHolder');
 const photoHolder = document.getElementById('photoHolder');
+
 const bitcoinHolder = document.getElementById('bitcoinHolder');
 
 
@@ -8,8 +9,11 @@ const auth = firebase.auth();
 
 auth.onAuthStateChanged(user => {
     console.log(user);
-    if(user.phoneNumber)
-        displayNameHolder.innerText = user.phoneNumber;
-        jinaHolder.innerText = user.phoneNumber;
-        bitcoinHolder.innerText = "Invoice to: " + " " + user.phoneNumber;
+    if(user.displayName)
+        displayNameHolder.innerText = user.displayName;
+        jinaHolder.innerText = user.displayName;
+    if(user.photoURL)
+        photoHolder.setAttribute('src', user.photoURL);
+    if(user.email)
+    bitcoinHolder.innerText = "Invoice to: " + " " + user.email ;
 })
