8454e4288937ea5b128feb4100a0369246860637
75 Business <75business@protonmail.com>
2021-07-02 09:56:04 -0700

    dsfhsdf

--- public/js/profile.js
+++ public/js/profile.js
@@ -8,8 +8,8 @@ const auth = firebase.auth();
 
 auth.onAuthStateChanged(user => {
     console.log(user);
-    if(user.phoneNumber)
-        displayNameHolder.innerText = user.phoneNumber;
-        jinaHolder.innerText = user.phoneNumber;
-        bitcoinHolder.innerText = "Invoice to: " + " " + user.phoneNumber;
+    if(user.displayNameHolder)
+        displayNameHolder.innerText = user.displayName;
+        jinaHolder.innerText = user.displayName;
+        bitcoinHolder.innerText = "Invoice to: " + " " + user.displayName;
 })
