851e5c34f4869f14a1c514502bd35ff278b8c8ba
75 Business <75business@protonmail.com>
2021-07-06 01:39:00 -0700

    Updates

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,3 @@
-const displayNameHolder = document.getElementById('displayNameHolder');
 const jinaHolder = document.getElementById('jinaHolder');
 const bitcoinHolder = document.getElementById('bitcoinHolder');
 const photoHolder = document.getElementById('photoHolder');
@@ -9,11 +8,10 @@ auth.onAuthStateChanged(user => {
     console.log(user);
     if(user.displayName)
         jinaHolder.innerText = user.displayName;
-        displayNameHolder.innerText = user.displayName;
     if(user.photoURL)
         photoHolder.setAttribute('src', user.photoURL);
     if(user.email)
-        bitcoinHolder.innerText = "Invoice to: " + " " + user.email;
+    bitcoinHolder.innerText = "Invoice to: " + " " + user.email;
 });
 
 
